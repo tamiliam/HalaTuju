8f1901d64b00e5b6830e47d64aea0921dbd5d864
Elanjelian Venugopal <tamiliam@gmail.com>
2026-01-17 14:33:51 +0800

    Rectified problems with Tamil translations 2.

--- main.py
+++ main.py
@@ -155,9 +155,9 @@ if 'dash' in st.session_state:
     if dash['total_matches'] > 0:
         st.success(t['hero_success'].format(count=dash['total_matches']))
         c1, c2, c3 = st.columns(3)
-        c1.metric(t['stat_poly'], dash['summary_stats'].get('inst_poly', 0))
-        c2.metric(t['stat_ikbn'], dash['summary_stats'].get('inst_ikbn', 0))
-        c3.metric(t['stat_kk'], dash['summary_stats'].get('inst_kk', 0))
+        c1.metric(t['inst_poly'], dash['summary_stats'].get('inst_poly', 0))
+        c2.metric(t['inst_ikbn'], dash['summary_stats'].get('inst_ikbn', 0))
+        c3.metric(t['inst_kk'], dash['summary_stats'].get('inst_kk', 0))
         st.markdown("---")
         
         if st.session_state['unlocked']:
